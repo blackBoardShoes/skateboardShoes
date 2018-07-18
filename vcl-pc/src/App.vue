c977a6f1d3af6e13dc86e18b1da3c9eb88595906
WrenchDE <1695408430@qq.com>
2018-07-18 10:36:41 +0800

    c

--- vcl-pc/src/App.vue
+++ vcl-pc/src/App.vue
@@ -5,20 +5,18 @@
 </template>
 
 <script>
-if (!process.env.IS_WEB) {
-  const {ipcRenderer} = window.require('electron')
-  console.log(ipcRenderer.sendSync('synchronous-message', 'ping'))
-  ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(this.$electron.ipcRenderer.sendSync('synchronous-message', 'ping'))
+  this.$electron.ipcRenderer.on('asynchronous-reply', (event, arg) => {
     console.log(arg)
   })
-  ipcRenderer.send('asynchronous-message', 'ping')
-}
+  this.$electron.ipcRenderer.send('asynchronous-message', 'ping')
 export default {
   name: 'App',
   methods: {
   },
   created () {
-    console.log(ipcRenderer)
+    console.log(this)
+    console.log(this.$electron)
   }
 }
 </script>
